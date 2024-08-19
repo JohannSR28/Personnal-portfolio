78a73df3e53eed7bc1d4ceb028dbb43d9d0cfdd2
JohannSR28 <souroujohann82@gmail.com>
2024-08-18 22:00:10 -0400

    french portfolio

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+function changeLanguage(lang) {
+  // Masquer toutes les sections de langue
+  const sections = document.querySelectorAll(".lang-section");
+  sections.forEach((section) => (section.style.display = "none"));
+
+  // Afficher la section pour la langue sélectionnée
+  const selectedSections = document.querySelectorAll(`.${lang}`);
+  selectedSections.forEach((section) => (section.style.display = "block"));
+}
+
+// Définir la langue par défaut
+changeLanguage("en");
